a024bb406e25b9622e4b44e24d51a5feeddeae69
Martin Honnen <martin.honnen@gmx.de>
2024-07-06 14:34:30 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Saxon.Api;
+
+var processor = new Processor(true);
+
+processor.SetProperty(Feature.XSD_VERSION, "1.0");
+
+processor.SchemaManager.Compile(new Uri(Path.Combine(Environment.CurrentDirectory, "schema1.xsd")));
+
+var docBuilder = processor.NewDocumentBuilder();
+
+docBuilder.SchemaValidator = processor.SchemaManager.NewSchemaValidator();
+
+var xmlDoc = docBuilder.Build(new Uri(Path.Combine(Environment.CurrentDirectory, "sample1.xml")));
+
+var xpathCompiler = processor.NewXPathCompiler();
+xpathCompiler.SchemaAware = true;
+
+foreach (XdmNode node in xpathCompiler.Evaluate("//*[not(*)]", xmlDoc))
+{
+    Console.WriteLine($"{node.NodeType} {node.NodeName} has typed value |{node.TypedValue}| of type {node.TypedValue.GetType()}");
+}
+
